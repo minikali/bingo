e562ae0a0e2bc4b8f228a2f3ebfe1a7df15f5527
minikali <remilak11@gmail.com>
2023-03-20 00:39:32 +0100

    refacto: Use Link instead of a

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import styles from "@/styles/Footer.module.css";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <div className={styles.footerLinks}>
-          <a href="/terms-of-use">Terms of Use</a>
-          <a href="/cookie-policy">Cookie Policy</a>
-          <a href="/disclaimer">Disclaimer</a>
+          <Link href="/terms-of-use">Terms of Use</Link>
+          <Link href="/cookie-policy">Cookie Policy</Link>
+          <Link href="/disclaimer">Disclaimer</Link>
         </div>
         <p className={styles.footerText}>
           Bingo Draw App &copy; 2023. All rights reserved.
